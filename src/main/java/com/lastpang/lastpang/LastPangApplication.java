4221f04b48b26a996d92d272b78b93b46050ec49
magae1 <jjilil8351@gmail.com>
2025-01-05 19:37:40 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/com/lastpang/lastpang/LastPangApplication.java
@@ -0,0 +1,13 @@
+package com.lastpang.lastpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastPangApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LastPangApplication.class, args);
+  }
+
+}
