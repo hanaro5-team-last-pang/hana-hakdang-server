bd508aa085d977491f247b64215c98ff424e6741
haneul0509 <haneul0509@naver.com>
2025-01-13 01:15:39 +0900

    :ambulance: [TASK-11] feat: 쿼리 추가

---
+++ src/main/java/com/hanahakdangserver/product/hanaitem/HanaItemRepository.java
@@ -0,0 +1,18 @@
+package com.hanahakdangserver.product.hanaitem;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface HanaItemRepository extends JpaRepository<HanaItem, Long> {
+  /**
+   * 태그 id로 HanaItem 리스트 조회
+   * @param tagId
+   * @return 해당 태그를 가진 HanaItem 리스트
+   */
+  @Query("SELECT h FROM HanaItem h WHERE h.tagId.id = :tagId")
+  List<HanaItem> findAllByTagId(@Param("tagId") Long tagId);
+
+}
---
+++ src/main/java/com/hanahakdangserver/user/careerinfo/CareerInfoRepository.java
@@ -0,0 +1,18 @@
+package com.hanahakdangserver.user.careerinfo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CareerInfoRepository extends JpaRepository<CareerInfo, Long> {
+
+  /**
+   * 멘토 id로 명함 조회
+   * @param mentorId
+   */
+  @Query("SELECT c FROM CareerInfo c WHERE c.mentorId.id = :mentorId")
+  Optional<CareerInfo> findByMentorId(@Param("mentorId") Long mentorId);
+
+}
