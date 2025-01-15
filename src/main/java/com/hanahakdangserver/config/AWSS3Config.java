5bb74e1c7c39ed22a5d47ec8d3fc43701bcbf411
Ji-eun Yang <jidak1165@gmail.com>
2025-01-15 10:31:06 +0900

    :rocket: [task-36] S3 Config 추가

---
+++ src/main/java/com/hanahakdangserver/config/AWSS3Config.java
@@ -0,0 +1,28 @@
+package com.hanahakdangserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+@Configuration
+public class AWSS3Config {
+  @Value("${aws.s3.accessKey}")
+  private String accessKey;
+  @Value("${aws.s3.secretKey}")
+  private String secretKey;
+
+
+  @Bean
+  public S3AsyncClient amazonS3AsyncClient() {
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+    return S3AsyncClient.builder()
+        .multipartEnabled(true)
+        .credentialsProvider(StaticCredentialsProvider.create(credentials))
+        .region(Region.AP_NORTHEAST_2)
+        .build();
+  }
+}
