362392a375ae51e540608bc4a89396568858b13a
commaengddongyeon <skdogrke1008!@>
2025-01-17 10:47:55 +0900

    :ambulance: 병합

--- src/test/java/com/hanahakdangserver/user/repository/UserRepositoryTest.java
+++ src/test/java/com/hanahakdangserver/user/repository/UserRepositoryTest.java
@@ -7,23 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import com.hanahakdangserver.config.AuthConfig;
 import com.hanahakdangserver.user.entity.User;
 import com.hanahakdangserver.user.enums.Role;
 
-
 @DataJpaTest
-@Import(AuthConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 
-  @Autowired
-  private PasswordEncoder passwordEncoder;
+  private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
   @Autowired
   private UserRepository userRepository;
 
