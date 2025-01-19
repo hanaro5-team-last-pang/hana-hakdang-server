5482c67c0b3241673a7737f2392c375104929edf
Ji-eun Yang <jidak1165@gmail.com>
2025-01-20 01:30:21 +0900

    :hammer: [task-42] 테스트 코드에서 유저 엔티티 부분을 제거

--- src/test/java/com/hanahakdangserver/faq/repository/FaqRepositoryTest.java
+++ src/test/java/com/hanahakdangserver/faq/repository/FaqRepositoryTest.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 import com.hanahakdangserver.classroom.entity.Classroom;
 import com.hanahakdangserver.classroom.repository.ClassroomRepository;
+import com.hanahakdangserver.config.ClockConfig;
+import com.hanahakdangserver.config.QueryDslConfig;
 import com.hanahakdangserver.faq.entity.Faq;
 import com.hanahakdangserver.lecture.entity.Category;
 import com.hanahakdangserver.lecture.entity.Lecture;
 import com.hanahakdangserver.lecture.repository.CategoryRepository;
 import com.hanahakdangserver.lecture.repository.LectureRepository;
 import com.hanahakdangserver.user.entity.CareerInfo;
-import com.hanahakdangserver.user.entity.User;
-import com.hanahakdangserver.user.enums.Role;
 import com.hanahakdangserver.user.repository.CareerInfoRepository;
 import com.hanahakdangserver.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,10 +22,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import({QueryDslConfig.class, ClockConfig.class}) // QueryDslConfig, ClockConfig를 가져옴
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class FaqRepositoryTest {
 
@@ -48,7 +50,7 @@ public class FaqRepositoryTest {
   private ClassroomRepository classroomRepository;
 
   private Lecture lecture;
-  private User user;
+//  private User user;
 
   @BeforeEach
   void setUp() {
@@ -68,15 +70,15 @@ public class FaqRepositoryTest {
     categoryRepository.save(category);
 
     // 유저 생성 및 저장
-    user = User.builder()
-        .name("Test User")
-        .email("test@example.com")
-        .password("password")
-        .birthDate(LocalDate.of(1990, 5, 20))
-        .role(Role.MENTEE)
-        .careerInfo(careerInfo)
-        .build();
-    userRepository.save(user);
+//    user = User.builder()
+//        .name("Test User")
+//        .email("test@example.com")
+//        .password("password")
+//        .birthDate(LocalDate.of(1990, 5, 20))
+//        .role(Role.MENTEE)
+//        .careerInfo(careerInfo)
+//        .build();
+//    userRepository.save(user);
 
     // 강의실 생성 및 저장
     Classroom classroom = Classroom.builder()
@@ -87,7 +89,7 @@ public class FaqRepositoryTest {
     // 강의 생성 및 저장
     lecture = Lecture.builder()
         .title("Test Lecture")
-        .mentor(user)
+//        .mentor(user)
         .classroom(classroom)
         .category(category)
         .startTime(LocalDateTime.now())
@@ -100,7 +102,7 @@ public class FaqRepositoryTest {
     // FAQ 생성 및 저장
     Faq faq = Faq.builder()
         .lecture(lecture)
-        .user(user)
+//        .user(user)
         .content("강의는 무엇을 배우나요???")
         .build();
     faqRepository.save(faq);
@@ -117,6 +119,6 @@ public class FaqRepositoryTest {
     assertThat(faqs).isNotEmpty();
     assertThat(faqs.get(0).getContent()).isEqualTo("강의는 무엇을 배우나요???");
     assertThat(faqs.get(0).getLecture().getTitle()).isEqualTo("Test Lecture");
-    assertThat(faqs.get(0).getUser().getName()).isEqualTo("Test User");
+//    assertThat(faqs.get(0).getUser().getName()).isEqualTo("Test User");
   }
 }
