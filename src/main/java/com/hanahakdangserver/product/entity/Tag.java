cf4237b91dc369fd175e5f63ec9d42d8cee85f6f
Ji-eun Yang <jidak1165@gmail.com>
2025-01-20 14:44:49 +0900

    :sparkles: [task-58] keyword를 포함하는 태그가 설정된 강의 검색 쿼리 추가

--- src/main/java/com/hanahakdangserver/product/entity/Tag.java
+++ src/main/java/com/hanahakdangserver/product/entity/Tag.java
@@ -1,15 +1,22 @@
 package com.hanahakdangserver.product.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import com.hanahakdangserver.lecture.entity.LectureTag;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,4 +31,7 @@ public class Tag {
 
   @Column(nullable = false, length = 255)
   private String tagName;
+
+  @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<LectureTag> lectureTagList;
 }
