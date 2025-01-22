cebe7313b16bfe164ee7a4ade1f9cbc309d1ca62
haneul0509 <haneul0509@naver.com>
2025-01-22 17:12:55 +0900

    :loud_sound: [task-73] 이메일 발송, 검증 로직 수정

--- src/main/java/com/hanahakdangserver/email/service/EmailService.java
+++ src/main/java/com/hanahakdangserver/email/service/EmailService.java
@@ -2,12 +2,15 @@ package com.hanahakdangserver.email.service;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -15,25 +18,20 @@ public class EmailService {
 
   private final JavaMailSender javaMailSender;
 
+  @Value("${front.port}")
+  private String port;
+
   @Async
   public void send(String email, String authToken) {
     MimeMessage mimeMessage = javaMailSender.createMimeMessage();
     try {
       MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
       helper.setTo(email);
-      helper.setSubject("하나학당 회원가입 인증 링크입니다.");
-
-      String htmlContent = "<html>" +
-          "<body>" +
-          "<p>회원가입을 완료하려면 아래 버튼을 클릭하세요✨</p>" +
-          "<form action='http://localhost:8080/verify-email' method='POST'>" +
-          "<input type='hidden' name='email' value='" + email + "' />" +
-          "<input type='hidden' name='authToken' value='" + authToken + "' />" +
-          "<button type='submit'>회원가입 인증</button>" +
-          "</form>" +
-          "</body>" +
-          "</html>";
-      helper.setText(htmlContent, true);
+      helper.setSubject("하나학당 회원가입 인증 링크입니다✨");
+//      String content = String.format("http://%s/verify-email?email=%s&authToken=%s", port,
+//          email, authToken);
+      String content = authToken; //임시
+      helper.setText(content, true);
 
       javaMailSender.send(mimeMessage);
 
