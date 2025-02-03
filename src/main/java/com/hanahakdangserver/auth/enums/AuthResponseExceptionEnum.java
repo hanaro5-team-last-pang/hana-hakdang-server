48925f52df23b56d7faff89da15955e4cd91bb92
Ji-eun Yang <jidak1165@gmail.com>
2025-02-03 21:36:26 +0900

    :recycle: [task-107] jwt 인증 추가

--- src/main/java/com/hanahakdangserver/auth/enums/AuthResponseExceptionEnum.java
+++ src/main/java/com/hanahakdangserver/auth/enums/AuthResponseExceptionEnum.java
@@ -18,7 +18,9 @@ public enum AuthResponseExceptionEnum {
   EMAIL_CHECK_EXPIRED(HttpStatus.BAD_REQUEST, "이메일 인증 시간이 만료됐습니다."),
   EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "이메일을 찾을 수 없습니다."),
   TOKEN_NOT_MATCHED(HttpStatus.BAD_REQUEST, "토큰이 일치하지 않습니다."),
-  USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요.");
+  USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해주세요."),
+  AUTHORIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "인증 처리 중 오류가 발생했습니다."),
+  UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
 
 
   private final HttpStatus httpStatus;
