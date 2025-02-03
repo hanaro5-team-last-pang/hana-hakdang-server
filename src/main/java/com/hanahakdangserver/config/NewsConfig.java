81a5f94396cfc6ff0f9fecd9573f4164dc8622da
commaengddongyeon <skdogrke1008!@>
2025-02-03 17:25:28 +0900

    :ambulance: 뉴스 크롤링 람다 연동 완료

--- src/main/java/com/hanahakdangserver/config/NewsConfig.java
+++ src/main/java/com/hanahakdangserver/config/NewsConfig.java
@@ -1,9 +1,13 @@
 package com.hanahakdangserver.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class NewsConfig {
@@ -18,6 +22,8 @@ public class NewsConfig {
             .defaultCodecs()
             .maxInMemorySize(10 * 1024 * 1024)) // 10MB로 설정
         .baseUrl(crawlingUrl)
+        .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+            .responseTimeout(Duration.ofSeconds(20))))
         .build();
   }
 }
